8b86112fe43f74bd6209aa146c9a41b1772c6fbf
Isobel <isobel.betteridge@gmail.com>
2019-05-17 16:41:55 +0100

    issue with signFactory fixed

--- GameView.java
+++ GameView.java
@@ -1,13 +1,25 @@
+/**
+ * Class to handle the bulk of the console output, leaving the game logic
+ * clear
+ */
+
 public class GameView {
 
+	/**
+	 * Displayed at the start of the game
+	 */
     public void introductionText() {
-
-        System.out.println("What do you choose?! \r\n");
+        System.out.println("What do you choose? \r\n");
         System.out.println("[Please enter 1, 2, 3, 4 or 5] \r\n");
         System.out.println("1: Rock \r\n2: Paper \r\n3: Scissors \r\n4: Spock \r\n5: Lizard");
-
     }
 
+    /**
+     * Displays text corresponding to who wins
+     * @param winningSign
+     * @param losingSign
+     * @param humanWin
+     */
     public void declareWinner(String winningSign, String losingSign, boolean humanWin) {
 
         if (humanWin)
--- SignFactory.java
+++ SignFactory.java
@@ -1,17 +1,22 @@
+/**
+ * Factory class to allow for simple creation of HandSign
+ * dependent on input
+ */
+
 public class SignFactory {
+	public HandSign getSign(int signType) {
 
-    public HandSign getSign(int signType) {
-
-        if (signType == 1) {
-            return new Rock();
-        } else if (signType == 2) {
-            return new Paper();
-        } else if (signType == 3) {
-            return new Paper();
-        } else if (signType == 4) {
-            return new Scissors();
-        } else if (signType == 5) {
-            return new Spock();
-        } else return null;
-    }
+		if (signType == 1) {
+			return new Rock();
+		} else if (signType == 2) {
+			return new Paper();
+		} else if (signType == 3) {
+			return new Scissors();
+		} else if (signType == 4) {
+			return new Spock();
+		} else if (signType == 5) {
+			return new Lizard();
+		} else
+			return null;
+	}
 }
