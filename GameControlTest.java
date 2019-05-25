a1b58789863216a779575f66ed622e9585e1e3be
Isobel <isobel.betteridge@gmail.com>
2019-05-25 10:36:00 +0100

    closed scanner, fixed a couple of minor bugs

--- GameControlTest.java
+++ GameControlTest.java
@@ -1,15 +1,11 @@
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//public class GameControlTest{
-//
-//    @Test
-//    public void decideWinnerLogicTest(){
-//    	HandSign rockSign = new Rock();
-//    	HandSign scissorsSign = new Scissors();
-//        GameView view = new GameView();
-//        GameControl control = new GameControl(view);
-//    	assertEquals("Human won",1,control.decideWinner(rockSign, scissorsSign));
-//    }
-//}
+import org.
+
+
+public class GameControlTest{
+
+    @Test
+    public void decideWinnerLogicTest(){
+
+
+    }
+}
