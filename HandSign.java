0c458075896fb413673f963b3a89ec9f55efedee
IsBeIsBe <46503459+IsBeIsBe@users.noreply.github.com>
2019-05-25 10:47:34 +0100

    Deleted redundant comment, added more detail

--- HandSign.java
+++ HandSign.java
@@ -1,19 +1,9 @@
+/*
+*  Interface to be applied to the different handsigns. Requires methods to retrieve name and ID
+*/
 public interface HandSign {
-
 
     public String getName();
     public int getID();
 
 }
-
-/*
- * The majority of such proposed generalizations are isomorphic to a simple game
- * of modular arithmetic, where half the differences are wins for player one.
- * For instance, rock-paper-scissors-Spock-lizard (note the different order of
- * the last two moves) may be modeled as a game in which each player picks a
- * number from one to five. Subtract the number chosen by player two from the
- * number chosen by player one, and then take the remainder modulo 5 of the
- * result. Player one is the victor if the difference is one or three, and
- * player two is the victor if the difference is two or four. If the difference
- * is zero, the game is a tie.
- */
